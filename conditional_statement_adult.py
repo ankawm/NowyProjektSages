6126c95be98386daf696989ab4a1fceebc8319a9
ankawm <56233568+ankawm@users.noreply.github.com>
2021-09-12 09:46:21 +0200

    zadania conditional gotowe

---
+++ conditional_statement_adult.py
@@ -0,0 +1,41 @@
+"""
+* Assignment: Conditional Statement Adult
+* Required: yes
+* Complexity: easy
+* Lines of code: 4 lines
+* Time: 3 min
+
+English:
+    1. Ask user to input age
+    2. Check whether user is adult (age above 18)
+    3. Run doctests - all must succeed
+
+Polish:
+    1. Poproś użytkownika o wprowadzenie wieku
+    2. Sprawdź czy użytkownik jest pełnoletni (wiek powyżej 18)
+    3. Uruchom doctesty - wszystkie muszą się powieść
+
+Tests:
+    >>> import sys; sys.tracebacklimit = 0
+
+    >>> assert result is not Ellipsis, \
+    'Assign result to variable: `result`'
+    >>> assert type(result) is str, \
+    'Variable `result` has invalid type, should be str'
+
+    >>> result in ('Adult', 'Young')
+    True
+"""
+
+from unittest.mock import MagicMock
+
+
+# Simulate user input (for test automation)
+input = MagicMock(side_effect=['21'])
+age = float(input('What is your age?: '))
+
+# str: Whether 'Adult' or 'Young'
+if age>=18:
+    result = "Adult"
+else:
+    result = "Young"
